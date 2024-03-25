4f157cd2d336eb38ebf201a4f75ea80c78f094cb
Muyizereberissa <muyizereberissa22@gmail.com>
2024-03-25 11:57:31 +0000

    web

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-
+# puppet manifest to rename a file and create a file
 
 exec { 'fix file name':
   command  => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
